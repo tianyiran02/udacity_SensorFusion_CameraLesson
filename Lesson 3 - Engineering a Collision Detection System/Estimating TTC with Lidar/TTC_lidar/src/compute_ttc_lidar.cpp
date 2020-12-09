49100dd7a16562880305374f5d27a6c511589c9c
Yiran Tian <tian.yiran@outlook.com>
2020-12-09 20:46:12 +0800

    complete lesson 3

--- Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
+++ Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
@@ -19,12 +19,18 @@ void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
     double minXPrev = 1e9, minXCurr = 1e9;
     for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
     {
-        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        if ((it->y >= -(laneWidth/2)) && (it->y <= (laneWidth/2)))
+        {
+            minXPrev = minXPrev > it->x ? it->x : minXPrev;
+        }
     }
 
     for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
     {
-        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        if ((it->y >= -(laneWidth/2)) && (it->y <= (laneWidth/2)))
+        {
+            minXCurr = minXCurr > it->x ? it->x : minXCurr;
+        }
     }
 
     // compute TTC from both measurements
