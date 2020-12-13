821195f3d5cadf43f4954829ba5c16d842c25b42
Yiran Tian <tian.yiran@outlook.com>
2020-12-13 15:13:54 +0800

    complete harris key point detection

--- Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
+++ Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
@@ -7,6 +7,41 @@
 
 using namespace std;
 
+static bool NMS(int column, int row, const cv::Mat & img, int limit)
+{
+    int temp, tempMax, tempCmp;
+    int searchColLow, searchRowLow, searchColHigh, searchRowHigh;
+
+    /* Determine the maximum search row and column */
+    temp = (limit - 1) / 2;
+
+    column >= temp ? searchColLow = column - temp : searchColLow = 0;
+    row >= temp ? searchRowLow = row - temp : searchRowLow = 0;
+
+    tempMax = column + temp;
+    tempMax > img.cols ? searchColHigh = (img.cols - 1) : searchColHigh = tempMax;
+    tempMax = row + temp;
+    tempMax > img.rows ? searchRowHigh = (img.rows - 1) : searchRowHigh = tempMax;
+
+    /* Now determine whether current point is local maximum */
+    temp = img.at<unsigned char>(column, row);
+    for (int i = searchColLow; i <= searchColHigh; i++)
+    {
+        for (int j = searchRowLow; j < searchRowHigh; j++)
+        {
+            tempCmp = img.at<unsigned char>(j, i);
+            if (tempCmp > temp)
+            {
+                /* Directly return if another point is larger */
+                return false;
+            }
+        }
+    }
+
+    std::cout << "Key point found!\n    column: " << column << "\n    row: " << row << std::endl;
+    return true;
+}
+
 void cornernessHarris()
 {
     // load image from file
@@ -27,6 +62,8 @@ void cornernessHarris()
     cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
     cv::convertScaleAbs(dst_norm, dst_norm_scaled);
 
+    std::cout << "Determine the type of data: " << dst_norm_scaled.type() << std::endl;
+
     // visualize results
     string windowName = "Harris Corner Detector Response Matrix";
     cv::namedWindow(windowName, 4);
@@ -37,7 +74,35 @@ void cornernessHarris()
     // and perform a non-maximum suppression (NMS) in a local neighborhood around 
     // each maximum. The resulting coordinates shall be stored in a list of keypoints 
     // of the type `vector<cv::KeyPoint>`.
+    int size = 3;   /* size must be odd, and greater than 1 (minimum 3) */
+    bool hit = false;
+    std::vector<cv::KeyPoint> logKeypoints;
 
+    std::cout << "total rows: " << dst_norm_scaled.rows << std::endl;
+    std::cout << "total columns: " << dst_norm_scaled.cols << std::endl;
+
+    for (int i = 0; i < dst_norm_scaled.rows; i++)
+    {
+        for (int j = 0; j < dst_norm_scaled.cols; j++)
+        {
+            if (dst_norm_scaled.at<unsigned char>(i, j) >= minResponse)
+            {
+                hit = NMS(i, j, dst_norm_scaled, size);
+                if(hit == true)
+                {
+                    hit = false;
+                    logKeypoints.push_back(cv::KeyPoint(j, i, size));
+                }
+            }   
+        }
+    }
+
+    windowName = "Harris Corner Detection Results";
+    cv::namedWindow(windowName, 5);
+    cv::Mat visImage = dst_norm_scaled.clone();
+    cv::drawKeypoints(dst_norm_scaled, logKeypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::imshow(windowName, visImage);
+    cv::waitKey(0);
 }
 
 int main()
